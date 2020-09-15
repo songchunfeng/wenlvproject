cf7591ba2eceede52e90809650fecdac9870da6d
ysy <youngshuyu1997@gmail.com>
2020-09-15 14:18:55 +0800

    .

--- src/view/person/home/list.vue
+++ src/view/person/home/list.vue
@@ -1,15 +1,206 @@
 <template>
-  <div>
-      个人景区列表
+  <div class="ListBox">
+    <div class="headBanner"></div>
+
+    <div class="noticeBox" @click="readShow = true">
+      <div class="noticeLeft">
+        <div class="iconRead">
+          <img src="../../../assets/images/预约须知.png" alt />
+        </div>
+        <div class="noticeText">预约须知</div>
+      </div>
+      <div class="noticeRight">
+        <img src="../../../assets/images/前往.png" alt />
+      </div>
+    </div>
+
+    <div class="listTitle">景区列表</div>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="getList">
+      <div class="List">
+        <div class="listItem">
+          <div class="scenicImg">
+            <img src="../../../assets/images/青海湖.png" alt />
+          </div>
+          <div class="scenicName">青海湖景区</div>
+        </div>
+        <div class="listItem">
+          <div class="scenicImg">
+            <img src="../../../assets/images/青海湖.png" alt />
+          </div>
+          <div class="scenicName">青海湖景区</div>
+        </div>
+        <div class="listItem">
+          <div class="scenicImg">
+            <img src="../../../assets/images/青海湖.png" alt />
+          </div>
+          <div class="scenicName">青海湖景区</div>
+        </div>
+        <div class="listItem">
+          <div class="scenicImg">
+            <img src="../../../assets/images/青海湖.png" alt />
+          </div>
+          <div class="scenicName">青海湖景区</div>
+        </div>
+      </div>
+    </van-list>
+    <!-- <van-loading class="top" v-show="loading" size="24px" color="#1989fa">加载中...</van-loading> -->
+
+    <van-popup
+      :safe-area-inset-bottom="true"
+      :overlay="false"
+      position="top"
+      v-model="readShow"
+      :style="{ height: '93%', width: '100%'}"
+    >
+      <read @close="closeRead"></read>
+    </van-popup>
   </div>
 </template>
 
 <script>
+import { Popup, Loading, List } from "vant";
+import read from "./readText";
 export default {
-
-}
+  name: "perList",
+  components: {
+    "van-popup": Popup,
+    "van-loading": Loading,
+    "van-list": List,
+    read,
+  },
+  data() {
+    return {
+      readShow: false,
+      list: [],
+      loading: false,
+      finished: false,
+    };
+  },
+  methods: {
+    closeRead() {
+      this.readShow = false;
+    },
+    getList () {
+      
+    }
+  },
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.ListBox {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .headBanner {
+    width: 100%;
+    height: 163px;
+    background: url("../../../assets/images/banner.png") no-repeat;
+    background-size: cover;
+  }
+  .noticeBox {
+    box-sizing: border-box;
+    margin-top: 15px;
+    width: 355px;
+    height: 59px;
+    background: #e9f3ff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 27px;
+    padding-right: 25px;
+    .noticeLeft {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      .iconRead {
+        width: 40px;
+        height: 39px;
+        margin-right: 16px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .noticeText {
+        font-size: 16px;
+        font-family: MicrosoftYaHei-Bold, MicrosoftYaHei;
+        font-weight: bold;
+        color: #3983e5;
+        line-height: 22px;
+        letter-spacing: 1px;
+      }
+    }
+    .noticeRight {
+      width: 7px;
+      height: 12px;
+      img {
+        width: 100%;
+        height: 100%;
+        vertical-align: top;
+      }
+    }
+  }
+  .listTitle {
+    width: 100%;
+    box-sizing: border-box;
+    padding-left: 10px;
+    margin-top: 22px;
+    font-size: 16px;
+    font-family: MicrosoftYaHei-Bold, MicrosoftYaHei;
+    font-weight: bold;
+    color: #333333;
+    line-height: 22px;
+    letter-spacing: 1px;
+  }
+  .List {
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    .listItem {
+      border: 1px solid red;
+      width: 173px;
+      min-height: 213px;
+      margin-top: 15px;
+      background: #ffffff;
+      box-shadow: 1px 1px 4px 0px rgba(0, 0, 0, 0.15);
+      border-radius: 5px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .scenicImg {
+        width: 100%;
+        height: 173px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .scenicName {
+        width: 100%;
+        padding: 9px 11px 13px 11px;
+        box-sizing: border-box;
+        flex: 1;
+        font-size: 15px;
+        font-family: MicrosoftYaHei;
+        color: #333333;
+        line-height: 19px;
+        letter-spacing: 1px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
+  }
+  .top {
+    margin-bottom: 50px;
+  }
+}
 </style>
---
+++ src/view/person/home/readText.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="readText">
+      <van-nav-bar fixed title="预约须知" @click-left="onClickLeft">
+        <template #left>
+          <img style="width:8px;height:14px" src="../../../assets/images/返回.png" alt />
+        </template>
+      </van-nav-bar>
+    
+    <div class="textContent">
+      <p>尊敬的游客：</p>
+      <p class="text">为确保您顺利完成景区门票预约操作，避免因操作失误给您带来不必要的麻烦，请认真阅读并确保您理解和接受以下条款：</p>
+      <p class="text">1、目前可通过“青海省文化和旅游厅官方网站”、“青海省文化旅游官方微信公众号”的制定入口“大美青海-青海省文化和旅游厅官方景区票务预约平台”，进入提前预约门票，以免耽误您的行程。</p>
+      <p class="text">2、游客需提前1-7天在“大美青海-青海省文化和旅游厅官方景区票务预约平台”进行实名预约。</p>
+      <p class="text">3、单笔门票预约订单最大预约人数为5人，预约时需按一人一证录入信息。团队票每次可预约20人，预约时需上传人员信息附件。</p>
+      <p class="text">4、预约成功后，系统自动向预留联系人发送预约成功消息通知，请妥善保存。</p>
+      <p class="text">5、电子票预约成功后，预约游览当日内使用有效。</p>
+      <p class="text">6、本预约平台仅提供门票预约服务，游客到达景区时，需在景区指定地点凭借预约成功短信、预约号码及预约订单等凭证，支付景区门票费用，兑换相应的景区门票后，检票入园。</p>
+      <p class="text">7、游客务必在预约时间、验票时间内抵达景区，请务必携带本人预约证件原件（身份证、护照）前往景区指定地点兑换景区门票；预约特殊票的游客需携带预约证件（身份证、护照），并额外携带优惠证件（如军官证、老年证、记者证等），未预约游客现场不售票。</p>
+      <p class="text">8、游客可提前1-7天在系统内改签或退订预约，多人订单可进行改签、退订。</p>
+      <p class="text">9、游客须在预约时间段到景区参观，如无法按时到达景区，需改签或退订本次预约，预约游览日当天无法改签，游客需自行发起退订申请，重新预约非当日门票。</p>
+      <p class="text">10、预约门票未兑票入园，逾期未能修改或取消，系统将自动默认该账号爽约1次，累计3次爽约的账号，将在30天内不能预约。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavBar } from "vant";
+export default {
+  name: "readText",
+  components: {
+    "van-nav-bar": NavBar,
+  },
+  methods: {
+    onClickLeft() {
+        this.$emit('close')
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.readText {
+  width: 100%;
+  position: relative;
+  .textContent {
+    width: 100%;
+    margin-top: 56px;
+    padding-left: 10px;
+    padding-right: 10px;
+    box-sizing: border-box;
+    p {
+      font-size: 15px;
+      font-family: MicrosoftYaHei;
+      color: #333333;
+      line-height: 24px;
+      letter-spacing: 1px;
+    }
+    .text {
+        text-indent: 2em;
+    }
+  }
+}
+</style>
